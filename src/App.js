6f62e8e3a1a4c55d281a92d5609e5106ad3be195
fabmansik <milansomyk@gmail.com>
2023-07-04 13:14:56 +0300

    hw1 Edited: - App.js - index.css - index.js Created: - RickAndMortyComponent.jsx - SimpsonsComponent.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import SimpsonsComponent from "./components/SimpsonsComponent";
+import RickAndMortyComponent from "./components/RickAndMortyComponent";
 function App() {
   return (
-    <div className="App">
+      <>
+          <h2> Simpsons components</h2>
+          <div className={'SimpsonsComponents'}>
+              <SimpsonsComponent person={1}/>
+              <SimpsonsComponent person={2}/>
+              <SimpsonsComponent person={3}/>
+              <SimpsonsComponent person={4}/>
+              <SimpsonsComponent person={5}/>
+          </div>
+          <h2>Rick And Morty components</h2>
+          <div className={'RickAndMortyComponents'}>
+              <RickAndMortyComponent person={1}/>
+              <RickAndMortyComponent person={2}/>
+              <RickAndMortyComponent person={3}/>
+              <RickAndMortyComponent person={4}/>
+              <RickAndMortyComponent person={5}/>
+              <RickAndMortyComponent person={6}/>
+          </div>
 
-    </div>
+        </>
+
   );
 }
 
